73077e956d1381a5ae82e4d5670182194d71d9ba
Esther Aragão <estheraragaos@gmail.com>
2020-08-23 19:01:27 -0300

    Registers product on url

--- ihair/urls.py
+++ ihair/urls.py
@@ -6,6 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'categories', views.CategoryViewSet)
+router.register(r'products', views.ProductViewSet)
 
 
 urlpatterns = [
