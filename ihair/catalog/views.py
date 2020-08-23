d5c1e1c26bf7d2121827f5db4bbd4a14ddb4f892
Esther Aragão <estheraragaos@gmail.com>
2020-08-23 19:57:47 -0300

    Adds CategoryList and CategoryDetail on views and replaces viewsets for generic views related to Category

--- ihair/catalog/views.py
+++ ihair/catalog/views.py
@@ -1,31 +1,12 @@
 from django.contrib.auth.models import User, Group
 from ihair.catalog.models import Category, Product
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import permissions, generics
 from ihair.catalog.serializers import UserSerializer, GroupSerializer, CategorySerializer, ProductSerializer
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer   
--- ihair/urls.py
+++ ihair/urls.py
@@ -3,13 +3,10 @@ from rest_framework import routers
 from ihair.catalog import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'products', views.ProductViewSet)
-
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view(), name='category-detail'),
 ]
