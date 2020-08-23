c12d6f7b1c4e1bfe4fbfea0c5c7f57fec9f34442
Esther Aragão <estheraragaos@gmail.com>
2020-08-23 16:11:09 -0300

    Adds class ProductSerializer

--- ihair/catolog/serializers.py
+++ ihair/catolog/serializers.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product 
+        fields = ['url', 'name', 'description', 'price', 'category']
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
