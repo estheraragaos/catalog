eee7336fe877be84e050485431628105e9b23109
Esther Aragão <estheraragaos@gmail.com>
2020-08-24 04:11:53 -0300

    Adds admin's route and formats urls file

--- ihair/urls.py
+++ ihair/urls.py
@@ -1,15 +1,18 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from ihair.catalog import views
+
 
 router = routers.DefaultRouter()
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', 
+    path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
+    path('admin/', admin.site.urls),
     path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), 
+    path('products/<int:pk>/', views.ProductDetail.as_view(),
          name='product-detail'),
     path('categories/', views.CategoryList.as_view()),
     path('categories/<int:pk>/', views.CategoryDetail.as_view(),
