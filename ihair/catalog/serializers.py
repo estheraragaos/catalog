493162ee802f985a923cf1b4d5745328fd90b4bb
Esther Aragão <estheraragaos@gmail.com>
2020-08-24 02:28:40 -0300

    Formats files

--- ihair/catalog/serializers.py
+++ ihair/catalog/serializers.py
@@ -2,15 +2,18 @@ from django.contrib.auth.models import User, Group
 from ihair.catalog.models import Category, Product
 from rest_framework import serializers
 
+
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product 
         fields = ['url', 'name', 'description', 'price', 'category']
 
+
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
         fields = ['url', 'name']
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
--- ihair/catalog/views.py
+++ ihair/catalog/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User, Group
 from ihair.catalog.models import Category, Product
 from rest_framework import permissions, generics, filters
-from ihair.catalog.serializers import UserSerializer, GroupSerializer, CategorySerializer, ProductSerializer
+from ihair.catalog.serializers import UserSerializer, GroupSerializer
+from ihair.catalog.serializers import CategorySerializer, ProductSerializer
+
 
 class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
@@ -9,15 +11,18 @@ class CategoryList(generics.ListCreateAPIView):
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
 
+
 class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer   
+
 
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['name','description', 'category__name']
+    search_fields = ['name', 'description', 'category__name']
+
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
